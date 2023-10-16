aa167420131c25afe8cda511b3c453566f5a02ea
Durgesh Sharma <109064132+Durge15@users.noreply.github.com>
2023-10-16 16:22:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
-document.addEventListener("DOMContentLoader" ,()=>{
+document.addEventListener("DOMContentLoader" ,() => {
 	document.body.innertext="DOM load success";
 });
